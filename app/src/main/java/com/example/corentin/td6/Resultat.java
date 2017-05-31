254898faad2ec04570ef21c8dada9f61244e3f57
Corentin MAY <corentin.may@live.fr>
2017-05-31 18:32:50 +0200

    commit json

--- app/src/main/java/com/example/corentin/td6/Resultat.java
+++ app/src/main/java/com/example/corentin/td6/Resultat.java
@@ -3,6 +3,17 @@ package com.example.corentin.td6;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Corentin on 31/05/2017.
@@ -15,9 +26,37 @@ public class Resultat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        Intent intent = this.getIntent();
-        intent.getStringExtra("Film");
+        final TextView textView = (TextView) findViewById(R.id.textView);
 
-        https://api.themoviedb.org/3/search/movie?api_key=67e311a7c21dbb5f13026fae8fc5dd0f&query=Jack+Reacher
+        Intent intent = this.getIntent();
+        String film = intent.getStringExtra("Film");
+        film = film.replace("\\s", "+");
+
+        String url = "https://api.themoviedb.org/3/search/movie?api_key=67e311a7c21dbb5f13026fae8fc5dd0f&query="+film;
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //try {
+                            textView.setText(response.toString());
+                      /*  } catch (JSONException e) {
+                            e.printStackTrace();
+                        }*/
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        queue.add(jsObjRequest);
     }
 }
