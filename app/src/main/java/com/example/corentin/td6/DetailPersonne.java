57906e97c7754011debcf9706ac927a32712819f
Corentin MAY <corentin.may@live.fr>
2017-06-04 12:46:42 +0200

    Clique sur film dans Personne

--- app/src/main/java/com/example/corentin/td6/DetailPersonne.java
+++ app/src/main/java/com/example/corentin/td6/DetailPersonne.java
@@ -1,7 +1,11 @@
 package com.example.corentin.td6;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
@@ -20,8 +24,7 @@ public class DetailPersonne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_personne);
-
-
+        
         Personne personne = this.getIntent().getExtras().getParcelable("Personne");
         ImageView poster = (ImageView) findViewById(R.id.imageViewDetailImagePersonne);
         Picasso.with(this.getApplicationContext()).load("https://image.tmdb.org/t/p/w500"+personne.getPoster()).into(poster);
@@ -33,7 +36,24 @@ public class DetailPersonne extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listview_detail_personne_list_film);
         listView.setAdapter(adapterResultFilm);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Media media = (Media) parent.getAdapter().getItem(position);
+                if (media.getType().contentEquals("Film")){
+                    Film film = (Film) media;
+                    Intent versSecondeActivity = new Intent(DetailPersonne.this, DetailFilm.class);
+                    versSecondeActivity.putExtra("Film", film);
+                    startActivity(versSecondeActivity);
+                } else if (media.getType().contentEquals("Serie")){
+                    Serie serie = (Serie) media;
+                    Intent versSecondeActivity = new Intent(DetailPersonne.this, DetailSerie.class);
+                    versSecondeActivity.putExtra("Serie", serie);
+                    startActivity(versSecondeActivity);
+                }
 
+            }
+        });
 
     }
 }
